29fdc1d37586a9b9ddf3d7d42eac864a2487c092
helenzzq <helenzhu0411@gmail.com>
2021-12-10 10:36:57 -0500

    Fix color adjusting issue and bid ask displaying error

--- price-tiles/src/app/components/currency-tile/trade-button/trade-button.component.ts
+++ price-tiles/src/app/components/currency-tile/trade-button/trade-button.component.ts
@@ -19,10 +19,11 @@ export class TradeButtonComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.price.tag);
-    this.tradeInfo.direction = this.price.tag;
+    
   }
 
   addTradeEntry() {
+    this.tradeInfo.direction = this.price.tag;
     this.tradeInfo.dealRate = this.price.wholeNum;
     this.displayTradeHistoryService.addTradeEntry(this.tradeInfo);
     console.log(this.tradeInfo);
