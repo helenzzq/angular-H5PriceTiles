61e21e5d1f239820419ae64adcbf5f206814c4e8
helenzzq <helenzhu0411@gmail.com>
2021-12-17 15:50:36 -0500

    add hover color change effect

--- price-tiles/src/app/components/currency-tile/trade-button/trade-button.component.ts
+++ price-tiles/src/app/components/currency-tile/trade-button/trade-button.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, Output } from '@angular/core';
 import { DisplayTradeHistoryService,   } from 'src/app/services/display-trade-history.service';
 import { TradeInfo } from 'src/app/model/trade-info.model';
 import { Price } from 'src/app/model/price.model';
-
+import * as $ from 'jquery'
 
 
 @Component({
@@ -18,7 +18,15 @@ export class TradeButtonComponent implements OnInit {
     DisplayTradeHistoryService) { }
 
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    var styleSelector = document.querySelector("body")
+    if (styleSelector) {
+      console.log(styleSelector.style.cssText);
+      $("body").css("--btn-active-color", "green");
+      // styleSelector.style.cssText = "--btn-active-color:green";
+    }
+    
+  }
 
   addTradeEntry() {
     this.tradeInfo.direction = this.price.tag;
